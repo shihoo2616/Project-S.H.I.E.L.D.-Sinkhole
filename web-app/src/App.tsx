43f40912499753f6a797a900f686af7b7a492330
shihoo2616 <shihoo2616@gmail.com>
2026-08-03 16:09:14 +0900

    Update App.tsx

--- web-app/src/App.tsx
+++ web-app/src/App.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import EscapeSRIPlannerPage from "./pages/EscapeSRIPlannerPage";
+import EscapeSRIPlannerPage from "./EscapeSRIPlannerPage";
 
 export default function App() {
   return (
